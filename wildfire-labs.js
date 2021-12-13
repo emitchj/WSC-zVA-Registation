21ac99da2ae13885d8326ddf2868771fb50e205b
kenishia <kenishia@us.ibm.com>
2021-12-13 16:18:04 -0500

    Update wildfire-labs.js

--- wildfire-labs.js
+++ wildfire-labs.js
@@ -2,7 +2,7 @@ var lines = [];
 
 $(document).ready(function () {
     $.ajax({
-        url: "labs_new.csv",
+        url: "labs.csv",
         dataType: "text",
         success: function (data) {
             var allTextLines = data.split(/\r\n|\n/);
