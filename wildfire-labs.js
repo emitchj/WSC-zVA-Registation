64d514def4fe9e66585bfafed017bdbe3d914e08
kenishia <kenishia@us.ibm.com>
2022-08-03 14:21:34 -0400

    updated to generic format

--- wildfire-labs.js
+++ wildfire-labs.js
@@ -26,7 +26,6 @@ $(document).ready(function () {
 function validate() {
     document.getElementById('btn-submit').disabled=(document.getElementById('registration-email').value=='');
 }
-//Edited the section below for SSA workshop.  uncomment lines 40 and 44-47 and comment line 39 to return.
 function getLab(email) {
     var hash = String(CryptoJS.MD5(email.trim().toLowerCase()));
     var index = -1;
@@ -36,15 +35,14 @@ function getLab(email) {
     var table_data = '<div class="alert alert-warning" role="alert">Email not found.</div>'
     if (index != -1) {
         table_data =  '<p>Below is the information you will need to access the exercises:</p>';
-        table_data += '<h5>Using a web browser (Preferred for SSA use)</h5>';
-  //      table_data += '<h5>Using a web browser </h5>';
+        table_data += '<h5>Using a web browser </h5>';
         table_data += '<p>Enter this URL address in the browser’s URL area: <br><mark>'+lines[index][1]+'</mark></p>';
         table_data += '<p>Authentication with username: <mark>'+lines[index][2]+'</mark></p>';
         table_data += '<p>Using password: <mark>'+lines[index][5]+'</mark></p>';
-  //      table_data += '<h5>Or using a Remote Desktop Connection <small class="text-muted">(Preferred)</small></h5>';
-  //      table_data += '<p>Enter in the Computer area this host name: <br><mark>'+lines[index][3]+'</mark></p>';
-  //      table_data += '<p>Follow the PDF instructions above and authenticate with username: <br><mark>'+lines[index][4]+'</mark></p>';
- //       table_data += '<p>Same password: <mark>'+lines[index][5]+'</mark></p>';
+        table_data += '<h5>Or using a Remote Desktop Connection <small class="text-muted">(Preferred)</small></h5>';
+        table_data += '<p>Enter in the Computer area this host name: <br><mark>'+lines[index][3]+'</mark></p>';
+        table_data += '<p>Follow the PDF instructions above and authenticate with username: <br><mark>'+lines[index][4]+'</mark></p>';
+        table_data += '<p>Same password: <mark>'+lines[index][5]+'</mark></p>';
 
         table_data += '<h5>During the lab</h5>';
         table_data += '<p>When the password for USER1 is required, enter <mark>USER1</mark>  (USER1 is a RACF identity)</p>';
